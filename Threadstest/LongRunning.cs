33808948bf53c56dd2038cb96b1e4a9b7fab59e5
Niels Schuler <niels.schuler@web.de>
2024-10-31 13:31:30 +0100

    Added Thread Testing

---
+++ Threadstest/LongRunning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threadstest
+{
+    public class LongRunning
+    {
+        private const int _secondMultiplier = 1000;
+        public static async Task<bool> LongRun(int Seconds)
+        {
+            await Task.Delay(Seconds * _secondMultiplier);
+            return true;
+        }
+
+        public static void Wait()
+        {
+            Thread.Sleep(30 * _secondMultiplier);
+        }
+    }
+}
--- Threadstest/Program.cs
+++ Threadstest/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Threadstest;
+
+Thread First = new Thread(new ThreadStart(LongRunning.Wait));
+Console.WriteLine("Created first");
+Thread Second = new Thread(new ThreadStart(LongRunning.Wait));
+Console.WriteLine("Created second");
+
+Console.WriteLine("Start first");
+First.Start();
+First.Join();
+Console.WriteLine("Start second");
+Second.Start();
+Second.Join();
